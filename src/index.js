e04e24a4fe32084ac838217bd1e1eb88bc86e5c7
MaksimZubrytski <69052702+MaksimZubrytski@users.noreply.github.com>
2021-01-28 09:49:44 +0300

    refactor: change code

--- src/index.js
+++ src/index.js
@@ -32,41 +32,11 @@ module.exports = function toReadable (number) {
     for(let i = 1; i < 10; i++) {
         newArray.push(newArray[90] + ' ' + newArray[i])
     }
-    newArray.push("one hundred")
-    for(let i = 1; i < 100; i++) {
-        newArray.push(newArray[100] + ' ' + newArray[i])
-    }
-    newArray.push("two hundred")
-    for(let i = 1; i < 100; i++) {
-        newArray.push(newArray[200] + ' ' + newArray[i])
-    }
-    newArray.push("three hundred")
-    for(let i = 1; i < 100; i++) {
-        newArray.push(newArray[300] + ' ' + newArray[i])
-    }
-    newArray.push("four hundred")
-    for(let i = 1; i < 100; i++) {
-        newArray.push(newArray[400] + ' ' + newArray[i])
-    }
-    newArray.push("five hundred")
-    for(let i = 1; i < 100; i++) {
-        newArray.push(newArray[500] + ' ' + newArray[i])
-    }
-    newArray.push("six hundred")
-    for(let i = 1; i < 100; i++) {
-        newArray.push(newArray[600] + ' ' + newArray[i])
-    }
-    newArray.push("seven hundred")
-    for(let i = 1; i < 100; i++) {
-        newArray.push(newArray[700] + ' ' + newArray[i])
-    }
-    newArray.push("eight hundred")
-    for(let i = 1; i < 100; i++) {
-        newArray.push(newArray[800] + ' ' + newArray[i])
-    }
-    newArray.push("nine hundred")
-    for(let i = 1; i < 100; i++) {
-        newArray.push(newArray[900] + ' ' + newArray[i])
+    for(let i = 1; i < 10; i++) {
+        newArray.push(newArray[i] + ' ' + 'hundred')
+        for(let j = 1; j < 100; j++) {
+            newArray.push(newArray[i] + ' ' + 'hundred' + ' ' + newArray[j])
+        }
     }
    return newArray[number]
 }
